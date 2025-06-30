92f7875b0481af37c2cf10f11d654592f8f29f95
Abhishek <a.k.jha0208@gmail.com>
2025-06-30 09:45:36 +0530

    First Commit

---
+++ TestProject/src/com/abhi/demo/FirstClass.java
@@ -0,0 +1,14 @@
+package com.abhi.demo;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		for (int i=1;i<=5;i++)
+		{
+			System.out.println("Welcome All..");
+		}
+
+	}
+
+}
